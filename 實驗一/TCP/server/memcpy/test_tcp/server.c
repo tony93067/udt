30feb4032b5d32c8063d0be77f5f38cbf98bfd79
tony93067 <tonyhuang0813@gmail.com>
2022-07-16 16:02:34 +0800

    20220716

--- 實驗一/TCP/server/memcpy/test_tcp/server.c
+++ 實驗一/TCP/server/memcpy/test_tcp/server.c
@@ -17,9 +17,12 @@
 #define PORT 1290
 #define BUFFER_SIZE 1500
 
+
 /***global value***/
 int num_packets = 0;
 int num_client = 1;
+int packet_size = 0;
+int packet_interval = 0;
 struct tms time_start,time_end;
 clock_t old_time, new_time;
 double ticks;
@@ -28,23 +31,28 @@ double ticks;
 
 void *send_packet(void *arg)
 {
-  int i = 0;
-  int j = 0;
-  char buffer[BUFFER_SIZE];
-  int send_size = 0;
-  int send_packet = 0;
-  int cd = *((int *)arg);
-  int no_client = num_client;
-  int total_sendsize = 0;
-  //int cd = (int *)&arg;
-  num_client++;
-  printf("Client %d: Start Sending Packet!\n",no_client);
-  printf("BUFFER_SIZE: %ld\n", sizeof(buffer));
-   /*make packet & send packet*/
-   i = 0;
-   for (j = 0; j < num_packets ; j++)//number of packet
-   {
-   		if(j == 0)
+    int fd;
+    int i = 0;
+    int j = 0;
+    char buffer[BUFFER_SIZE];
+    int send_size = 0;
+    int send_packet = 0;
+    int cd = *((int *)arg);
+    int no_client = num_client;
+    int total_sendsize = 0;
+    int return_value = 0;
+    //int cd = (int *)&arg;
+    num_client++;
+    printf("Client %d: Start Sending Packet!\n",no_client);
+    printf("Packet Size: %ld\n", sizeof(buffer));
+    /*make packet & send packet*/
+    i = 0;
+    // open file
+    fd = open("/home/tony/論文code/論文code/file.txt", O_RDONLY, S_IRWXU);
+    while(1)
+    {
+        memset(buffer, '\0', BUFFER_SIZE);
+        if(j == 0)
    		{
    			if((old_time = times(&time_start)) == -1)
       		{
@@ -52,120 +60,106 @@ void *send_packet(void *arg)
         		exit(1);
       		}
    		}
-      //fill data in packet
-      if(i > 255)//data: 0-255
-        i = 0;
+        if((return_value = read(fd, buffer, packet_size)) == -1)
+        {
+            printf("Read error\n");
+            exit(1);
+        }
+        if(return_value == 0)
+            break;
+        //send packet
+        if((send_size = send(cd, (char *)buffer, return_value, 0)) < 0)
+        {
+            DIE("send");
+        }
+        //printf("send size %d\n", send_size);
+        send_packet++;
+        total_sendsize += send_size;
+        // sleep 0.025s(use to control sending rate)
+        usleep(packet_interval);
+    }
+    close(fd);
+    if((new_time = times(&time_end)) == -1)
+    {
+        printf("time error\n");
+        exit(1);
+    }
+    ticks=sysconf(_SC_CLK_TCK);
+    double execute_time = (new_time - old_time)/ticks;
+    printf("Execute Time: %2.2f\n", execute_time);
+    printf("total sendsize %d \n", total_sendsize);
+    /*************/
+    printf("Client %d: Total Send Packet: %d\n",no_client,send_packet);
+    printf("Client %d: Packet send sucessfully!\n\n",no_client);	
 
-      memset(buffer,i,sizeof(buffer));//fill data in buffer
-      i++;//next data
-
-      //send packet
-      if((send_size = send(cd,(char *)buffer,sizeof(buffer),0)) < 0)
-      {
-        DIE("send");
-      }
-      //printf("send size %d\n", send_size);
-      send_packet++;
-<<<<<<< HEAD
-      total_sendsize += send_size;
-      // sleep 0.025s(use to control sending rate)
-     usleep(1000);
-   }
-   if((new_time = times(&time_end)) == -1)
-   {
-       printf("time error\n");
-       exit(1);
-=======
-      
-      // sleep 0.025s(use to control sending rate)
-     usleep(1000);
->>>>>>> 827f7dfa0690ed4c487fdc0e3a4f266a51524766
-   }
-   ticks=sysconf(_SC_CLK_TCK);
-   double execute_time = (new_time - old_time)/ticks;
-   printf("Execute Time: %2.2f\n", execute_time);
-   printf("total sendsize %d \n", total_sendsize);
-   /*************/
-   printf("Client %d: Total Send Packet: %d\n",no_client,send_packet);
-   printf("Client %d: Packet send sucessfully!\n\n",no_client);	
-
-   //close connection
-   close(cd);
-   pthread_exit(0);
+    //close connection
+    close(cd);
+    pthread_exit(0);
 }
 
 int main(int argc, char **argv)
 {
-   static struct sockaddr_in server;
-   int sd,cd; 
-   int reuseaddr = 1;
+    static struct sockaddr_in server;
+    int sd,cd; 
+    int reuseaddr = 1;
 //   char buffer[BUFFER_SIZE];
 //   int send_size = 0;
-   int client_len = sizeof(struct sockaddr_in);
+    int client_len = sizeof(struct sockaddr_in);
 //   int i = 0,j = 0;
 //   int send_packet = 0;
-   pthread_t p1;
-   int ret = 0;
+    pthread_t p1;
+    int ret = 0;
 //   pthread_mutex_t work = PTHREAD_MUTEX_INITIALIZER;
 
-   if(argc != 2)
-   {
-     printf("Usage: %s <num_packets>\n",argv[0]);
-     exit(1);
-   }
+    if(argc != 4)
+    {
+        printf("Usage: %s <num_packets> <packet_size> <packet_interval(us)>\n",argv[0]);
+        exit(1);
+    }
 
-   //open socket
-   sd = socket(AF_INET,SOCK_STREAM,0);
-   if(sd < 0)
-   {
-     DIE("socket");
-   }
+    //open socket
+    sd = socket(AF_INET,SOCK_STREAM,0);
+    if(sd < 0)
+    {
+        DIE("socket");
+    }
 
-   /* Initialize address. */
-   server.sin_family = AF_INET;
-   server.sin_port = htons(PORT);
-   server.sin_addr.s_addr = htonl(INADDR_ANY);
+    /* Initialize address. */
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-   //reuse address
-   setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,&reuseaddr,sizeof(reuseaddr));
+    //reuse address
+    setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,&reuseaddr,sizeof(reuseaddr));
 
-   //bind
-   if(bind(sd,(struct sockaddr *)&server,sizeof(server)) < 0)
-   {
-     DIE("bind");
-   }
+    //bind
+    if(bind(sd,(struct sockaddr *)&server,sizeof(server)) < 0)
+    {
+        DIE("bind");
+    }
 
-   //listen
-   if(listen(sd,1) < 0)
-   {
-     DIE("listen");
-   }
+    //listen
+    if(listen(sd,1) < 0)
+    {
+        DIE("listen");
+    }
    
-   num_packets = atoi(argv[1]);
+    num_packets = atoi(argv[1]);
+    packet_size = atoi(argv[2]);
+    packet_interval = atoi(argv[3]);
 
-   while(1)
-   {
-   //accept
-/*   if((cd = accept(sd,(struct sockaddr *)&server,&client_len)) == -1)
-   {
-     DIE("accept");
-   }
-*/
-//    pthread_mutex_lock(&work); 
-     cd = accept(sd,(struct sockaddr *)&server,&client_len);
-//    pthread_mutex_unlock(&work);
-//     run_times = atoi(argv[1]);
+    while(1)
+    {
+        cd = accept(sd,(struct sockaddr *)&server,&client_len);
 
-//   printf("Start Sending!\nPacket Data Size: %d\n",(int)sizeof(buffer));
-   /*create thread to handle each client, one receiver use one different protocol*/
-     if((ret = pthread_create(&p1, NULL, send_packet, (void*)&cd)) != 0)
-     {
-       fprintf(stderr, "can't create p1 thread:%s\n", strerror(ret));
-       exit(1);
-     }
-   }
-   //close connection
-   close(sd);
+        if((ret = pthread_create(&p1, NULL, send_packet, (void*)&cd)) != 0)
+        {
+        fprintf(stderr, "can't create p1 thread:%s\n", strerror(ret));
+        exit(1);
+        }
+    }
+    //close connection
+    close(sd);
   
   return 0;
 }
